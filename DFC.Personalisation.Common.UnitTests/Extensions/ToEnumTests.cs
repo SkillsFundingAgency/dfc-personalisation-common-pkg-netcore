fe5cd3023d6b3166d9bec67fbebd03ca810c0b49
Liam Wright <mail@liam-wright.co.uk>
2020-01-02 14:09:24 +0000

    Added extension method, and unit tests

---
+++ DFC.Personalisation.Common.UnitTests/Extensions/ToEnumTests.cs
@@ -0,0 +1,44 @@
+﻿using DFC.Personalisation.Common.Extensions;
+using NUnit.Framework;
+
+namespace DFC.Personalisation.Common.UnitTests.Extenisons
+{
+    internal enum TestEnums
+    {
+        Undefined = 0,
+        Test1 = 1
+    }
+    public class ToEnumTests
+    {
+        [Test]
+        public void If_Invalid_Value_Return_Default()
+        {
+            string incorrect = "sdfdfsfs";
+            var result = incorrect.ToEnum(defaultValue: TestEnums.Undefined);
+            Assert.AreEqual(TestEnums.Undefined, result);
+        }
+
+        [Test]
+        public void If_Blank_Value_Return_Default()
+        {
+            var blank = string.Empty;
+            var result = blank.ToEnum(defaultValue: TestEnums.Undefined);
+            Assert.AreEqual(TestEnums.Undefined, result);
+        }
+
+        [Test]
+        public void If_Valid_Value_Return_Correct_Value()
+        {
+            var validTest1 = "Test1";
+            var result = validTest1.ToEnum(defaultValue: TestEnums.Undefined);
+            Assert.AreEqual(TestEnums.Test1, result);
+        }
+        [Test]
+        public void If_Valid_CaseInsensitive_Value_Return_Correct_Value()
+        {
+            var validTest1 = "TeSt1";
+            var result = validTest1.ToEnum(defaultValue: TestEnums.Undefined);
+            Assert.AreEqual(TestEnums.Test1, result);
+        }
+    }
+}
---
+++ DFC.Personalisation.Common/Extensions/ToEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DFC.Personalisation.Common.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T ToEnum<T>(this string value, T defaultValue) where T : struct
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return defaultValue;
+            }
+            return Enum.TryParse<T>(value, true, out T result) ? result : defaultValue;
+        }
+    }
+}
