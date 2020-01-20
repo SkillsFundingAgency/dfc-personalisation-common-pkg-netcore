5fec01715a5786a7c9da55e8788b2e0939aee49a
Stephen Yates <SYatesSoftware@gmail.com>
2020-01-20 14:28:06 +0000

    Added FirstCharToUpper Extension

---
+++ DFC.Personalisation.Common.UnitTests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using DFC.Personalisation.Common.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DFC.Personalisation.Common.UnitTests.Extensions
+{
+    public class StringExtensionTests
+    {
+        [Test]
+        public void When_FirstCharToUpperCalled_Then_TheStringReturnedShouldHaveAnUpperCaseFirstChar()
+        {
+            const string testString = "test";
+
+            testString.FirstCharToUpper().Should().Be("Test");
+
+        }
+    }
+}
