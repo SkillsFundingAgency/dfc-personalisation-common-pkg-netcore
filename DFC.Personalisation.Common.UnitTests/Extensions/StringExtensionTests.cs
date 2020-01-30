381584f307c4e66cd28bd9e7bd5909dc380c351a
Matt Daniels <matt@techtoniq.com>
2020-01-30 12:44:56 +0000

    Add string extension to capitalise first letter

---
+++ DFC.Personalisation.Common.UnitTests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using DFC.Personalisation.Common.Extensions;
+using FluentAssertions;
+
+namespace DFC.Personalisation.Common.UnitTests.Extensions
+{
+    public class StringExtensionTests
+    {
+        [Test]
+        public void When_Null_Then_ReturnEmptyString()
+        {
+            // Arrange.
+            string sourceString = null;
+
+            // Act.
+            var result = sourceString.UppercaseFirst();
+
+            // Assert.
+            result.Should().BeEmpty();
+
+        }
+
+        [Test]
+        public void When_Empty_Then_ReturnEmptyString()
+        {
+            // Arrange.
+            string sourceString = string.Empty;
+
+            // Act.
+            var result = sourceString.UppercaseFirst();
+
+            // Assert.
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void When_NonEmpty_Then_ReturnFirstCharacterCapitalised()
+        {
+            // Arrange.
+            string sourceString = "wibble";
+
+            // Act.
+            var result = sourceString.UppercaseFirst();
+
+            // Assert.
+            result.Should().Be("Wibble");
+        }
+    }
+}
